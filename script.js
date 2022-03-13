ab1cf6269a38079306e6aa44c6c8dfc8cc19bb9b
Marin Nikolli <sibalonat@gmail.com>
2022-03-13 20:32:14 +0100

    moire animation

--- script.js
+++ script.js
@@ -42,50 +42,41 @@ const two = new Two(params);
 
 two.appendTo(container);
 
-const numberOfShapes = 25;
+const numberOfShapes = 50;
 
-const loopDuration = 6 * 60;
+
+const startWidth = 50;
+const endWidth = 500;
+const difWidth = endWidth - startWidth;
+const startRotation = 0;
+const endRotation = fullRotation * 6 / 360;
+
+
+const loopDuration = 12 * 60;
+const aDelay = 0.001;
 
 const shapes = [];
 
 aDelay = 0.0025;
 
 
-//bezier animation and shapes
+//moire animation and shapes
 for (let i = 0; i < numberOfShapes; i++) {
 
-    const size = 20;
-    const sx = size * i + 10;
-    const sy = 250;
-    const sr = 0;
-    // const ss = 1;
+    const x = 250;
+    let y = i * 20 + 5;
 
-    //with plotRadius
-    // const plotRadius = 200;
-    // const angle = fullRotation * i / numberOfShapes;
-    // const ex = 250 + plotRadius * Math.cos(angle); 
-    // const ey = 250 + plotRadius * Math.sin(angle);
-    // before plotRadius change
-    const ex = randomNumber(50, 450);
-    const ey = randomNumber(50, 450);
-    const er = randomNumber(-2 * fullRotation, 2 * fullRotation);
-    // const es = randomNumber(0.5, 1.4);
-
-
-    // const size = (numberOfShapes - i) * shapeIncr;
-
-    const shape = two.makeRectangle(sx, sy, size, size);
-    shape.fill = '#004f73';
-    shape.data = {
-        sx: sx,
-        sy: sy,
-        sr: sr,
-        // ss: ss,
-        ex: ex,
-        ey: ey,
-        er: er,
-        // es: es
+    if (i >= 0.25) {
+        y -= 490;
     }
+
+    const shape = two.makeRectangle(x, y, startWidth, 10);
+    shape.fill = '#5645d3';
+
+    if (i >= 25) {
+        shape.fill = '#99e6e0';        
+    }
+
     shape.noStroke();
 
     shapes.push(shape);
@@ -94,73 +85,175 @@ for (let i = 0; i < numberOfShapes; i++) {
 
 
 
-//bezier animation and shapes
+//moire animation and shapes
 two.bind('update', function (frameCount) {
     const currentFrame = frameCount % loopDuration;
     const t = currentFrame / loopDuration;
     shapes.forEach((shape, i) => {
+        let r = startRotation;
+        let w = startWidth;
 
-        if (currentFrame === 0) {
-            //change this this properties on reload of the page
-            shape.data.ex = randomNumber(50, 450);
-            shape.data.ey = randomNumber(50, 450);
-            shape.data.er = randomNumber(-2 * fullRotation, 2 * fullRotation);
+        let aStart = aDelay * i;
+        let aEnd = aDelay * (numberOfShapes - i);
+
+        if (i >= 0.25) {
+            aStart = aDelay * (numberOfShapes - i);
+            aEnd = aDelay * i;
         }
-        const aStart = aDelay * (numberOfShapes - i);
-        const aEnd = aDelay * i; 
 
-        let u = 0;
-
-        if (t < 0.5) {
-            u = mapAndClamp(t, aStart, 0.5 - aEnd, 0, 1);            
+        if (t < 0.25) {
+            const u = mapAndClamp(t, 0 + aStart, 0.25 - aEnd, 0, 1);
+            const cu = easeInOutCubic(u);
+            w = mapAndClamp(cu, 0, 1, startWidth, endWidth);
+            r = startRotation;
+        } else if(t < 0.5) {
+            const u = mapAndClamp(t, 0.25 + aStart, 0.5 - aEnd, 0, 1);
+            w = endWidth;
+            const cu = easeInOutCubic(u);
+            r = mapAndClamp(cu, 0, 1, startRotation, endRotation);
+        } else if(t < 0.75) {
+            const u = mapAndClamp(t, 0.5 + aStart, 0.75 - aEnd, 0, 1);
+            const cu = easeInOutCubic(u);
+            w = mapAndClamp(cu, 0, 1, endWidth, startWidth);
+            r = startRotation;
         } else {
-            u = mapAndClamp(t, 0.5 + aStart, 1 - aEnd, 1, 0);
+            w = startWidth;
+            const u = mapAndClamp(t, 0.75 + aStart, 1 - aEnd, 0, 1);
+            const cu = easeInOutCubic(u);
+            r = mapAndClamp(cu, 0, 1, endRotation, startRotation);
         }
-
-        const cu = easeInOutCubic(u);
-
-        const x = mapAndClamp(cu, 0, 1, shape.data.sx, shape.data.ex);
-        const y = mapAndClamp(cu, 0, 1, shape.data.sy, shape.data.ey);
-        const r = mapAndClamp(cu, 0, 1, shape.data.sr, shape.data.er);
-        // const s = mapAndClamp(cu, 0, 1, shape.data.ss, shape.data.es);
-        shape.translation.x = x;
-        shape.translation.y = y;
+        shape.width = w;
         shape.rotation = r;
-        // shape.scale = s;
+
+        if (i >= 25) {
+            shape.rotation = r * -1            
+        }
     })
 })
-
-let currentColor = 0;
-
-const bgColors = [
-    '#45d3c5',
-    '#ffe8b4',
-    '#f9d2cd',
-    '#bcdffd'
-];
-
-const shapeColors = [
-    '#004f73',
-    '#f8bc30',
-    '#f45745',
-    '#5745d3'
-];
-
-document.addEventListener('click', () => {
-    currentColor += 1;
-    currentColor = currentColor % bgColors.length;
-
-    const bodyTag = document.querySelector('body');
-    bodyTag.style.backgroundColor = bgColors[currentColor];
-
-    shapes.forEach((shape, i) => {
-        shape.fill = shapeColors[currentColor];
-    })
-})
-
 
 
 two.play();
+
+// project 4
+// const numberOfShapes = 25;
+
+// const loopDuration = 6 * 60;
+
+// const shapes = [];
+
+// aDelay = 0.0025;
+
+
+// //bezier animation and shapes
+// for (let i = 0; i < numberOfShapes; i++) {
+
+//     const size = 20;
+//     const sx = size * i + 10;
+//     const sy = 250;
+//     const sr = 0;
+//     // const ss = 1;
+
+//     //with plotRadius
+//     // const plotRadius = 200;
+//     // const angle = fullRotation * i / numberOfShapes;
+//     // const ex = 250 + plotRadius * Math.cos(angle); 
+//     // const ey = 250 + plotRadius * Math.sin(angle);
+//     // before plotRadius change
+//     const ex = randomNumber(50, 450);
+//     const ey = randomNumber(50, 450);
+//     const er = randomNumber(-2 * fullRotation, 2 * fullRotation);
+//     // const es = randomNumber(0.5, 1.4);
+
+
+//     // const size = (numberOfShapes - i) * shapeIncr;
+
+//     const shape = two.makeRectangle(sx, sy, size, size);
+//     shape.fill = '#004f73';
+//     shape.data = {
+//         sx: sx,
+//         sy: sy,
+//         sr: sr,
+//         // ss: ss,
+//         ex: ex,
+//         ey: ey,
+//         er: er,
+//         // es: es
+//     }
+//     shape.noStroke();
+
+//     shapes.push(shape);
+
+// }
+
+
+
+// // random scater, scaling, and aligning
+// two.bind('update', function (frameCount) {
+//     const currentFrame = frameCount % loopDuration;
+//     const t = currentFrame / loopDuration;
+//     shapes.forEach((shape, i) => {
+
+//         if (currentFrame === 0) {
+//             //change this this properties on reload of the page
+//             shape.data.ex = randomNumber(50, 450);
+//             shape.data.ey = randomNumber(50, 450);
+//             shape.data.er = randomNumber(-2 * fullRotation, 2 * fullRotation);
+//         }
+//         const aStart = aDelay * (numberOfShapes - i);
+//         const aEnd = aDelay * i; 
+
+//         let u = 0;
+
+//         if (t < 0.5) {
+//             u = mapAndClamp(t, aStart, 0.5 - aEnd, 0, 1);            
+//         } else {
+//             u = mapAndClamp(t, 0.5 + aStart, 1 - aEnd, 1, 0);
+//         }
+
+//         const cu = easeInOutCubic(u);
+
+//         const x = mapAndClamp(cu, 0, 1, shape.data.sx, shape.data.ex);
+//         const y = mapAndClamp(cu, 0, 1, shape.data.sy, shape.data.ey);
+//         const r = mapAndClamp(cu, 0, 1, shape.data.sr, shape.data.er);
+//         // const s = mapAndClamp(cu, 0, 1, shape.data.ss, shape.data.es);
+//         shape.translation.x = x;
+//         shape.translation.y = y;
+//         shape.rotation = r;
+//         // shape.scale = s;
+//     })
+// })
+
+// let currentColor = 0;
+
+// const bgColors = [
+//     '#45d3c5',
+//     '#ffe8b4',
+//     '#f9d2cd',
+//     '#bcdffd'
+// ];
+
+// const shapeColors = [
+//     '#004f73',
+//     '#f8bc30',
+//     '#f45745',
+//     '#5745d3'
+// ];
+
+// document.addEventListener('click', () => {
+//     currentColor += 1;
+//     currentColor = currentColor % bgColors.length;
+
+//     const bodyTag = document.querySelector('body');
+//     bodyTag.style.backgroundColor = bgColors[currentColor];
+
+//     shapes.forEach((shape, i) => {
+//         shape.fill = shapeColors[currentColor];
+//     })
+// })
+
+
+
+// two.play();
 
 // mouse interaction animation and pivot looks animation
 // const numberOfShapes = 25;
